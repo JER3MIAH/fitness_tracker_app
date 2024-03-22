dbe365183c00481e742eb45117e2516a99b54f1c
JER3MIAH <jeremiahbrownie@gmail.com>
2024-03-22 13:17:29 +0100

    Remove exposed secrets

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:fitness_tracker_app/src/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
-
-
--- lib/src/shared/utils/strings.dart
+++ lib/src/shared/utils/strings.dart
@@ -6,9 +6,6 @@ const kDefaultError = 'Oops something went wrong!';
 // font families
 const String kNunito = 'Nunito';
 
-//google maps
-const String kGoogleApiKey = "AIzaSyDDKzBMT4gcP0zjBDQib2MeaVWLllWzxoQ";
-
 // auth
 const String verifyPhoneType = "VERIFY_PHONE";
 const String resetPasswordType = "RESET_PASSWORD";
